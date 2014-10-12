0b868ce062d2558ad24aff4bd4310b44ccfd8eb7
vansar <vansar.w@gmail.com>
2014-10-12 22:58:44 +0800

    Create Project

---
+++ MyMusicPlayer/MyMusicPlayer/MyMusicPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyMusicPlayer
+//
+//  Created by vansar on 14-10-12.
+//  Copyright (c) 2014年 vansar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
